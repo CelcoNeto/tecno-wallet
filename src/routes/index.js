a42a313bb684db430d8fe60eeb3bdfd31ff6c7a2
celco.neto <celco.neto@devninjas.com.br>
2020-12-27 16:45:35 -0300

    fix: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,33 @@
 import Router from "express";
+import multer from "multer";
 import swaggerUi from "swagger-ui-express";
+import { categoriesController } from "../app/controllers/categories-controller";
+import { fileController } from "../app/controllers/file-controller";
+import { sessionController } from "../app/controllers/session-controller";
+import { transactionsController } from "../app/controllers/transactions-controller";
+import { userController } from "../app/controllers/user-controller";
+import multerConfig from "../config/multer";
+import authMiddlware from "../middlewares/auth";
 import swaggerDocument from "../swagger.config.json";
-import routesCategories from "./categories";
-import routesUsers from "./users";
-import routesTransactions from "./transactions";
+
 const routes = new Router();
+const upload = multer(multerConfig);
+
 routes.use("/api/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-routes.use(routesUsers);
-routes.use(routesCategories);
-routes.use(routesTransactions);
+routes.post("/api/users", userController.store);
+routes.post("/api/files", upload.single("file"), fileController.store);
+routes.post("/api/login", sessionController.store);
+
+routes.use(authMiddlware);
+
+routes.put("/api/users", userController.update);
+
+routes.get("/api/categories", categoriesController.showAll);
+routes.post("/api/categories", categoriesController.store);
+routes.get("/api/categories/:id", categoriesController.show);
+routes.put("/api/categories/:id", categoriesController.update);
+routes.get("/api/resume", transactionsController.resume);
+
+routes.post("/api/transactions", transactionsController.store);
 
 export { routes };
